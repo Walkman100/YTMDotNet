690fd0c5fe1768bed70aa352cd44812be991de76
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-07-11 16:37:51 +0200

    added project

---
+++ Properties/Application.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YTMDotNet {
+    static class Application {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
