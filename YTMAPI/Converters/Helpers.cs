b220afc8ce9fe0292f91564717a4f574646e4299
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-07-17 15:33:49 +0200

    added python dict/list to .Net converter

---
+++ YTMAPI/Converters/Helpers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace YTMDotNet.YTMAPI.Converters {
+    static class Helpers {
+        /// <summary>Duplicate of the <see cref="int.TryParse(string, out int)"/> method except with <see cref="NumberFormatInfo.InvariantInfo"/> instead of <see cref="NumberFormatInfo.CurrentInfo"/></summary>
+        public static bool TryParse(string s, out int result) =>
+            int.TryParse(s, NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out result);
+        /// <summary>Duplicate of the <see cref="ulong.TryParse(string, out ulong)"/> method except with <see cref="NumberFormatInfo.InvariantInfo"/> instead of <see cref="NumberFormatInfo.CurrentInfo"/></summary>
+        public static bool TryParse(string s, out ulong result) =>
+            ulong.TryParse(s, NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out result);
+        /// <summary>Duplicate of the <see cref="double.TryParse(string, out double)"/> method except with <see cref="NumberFormatInfo.InvariantInfo"/> instead of <see cref="NumberFormatInfo.CurrentInfo"/></summary>
+        public static bool TryParse(string s, out double result) =>
+            double.TryParse(s, NumberStyles.AllowLeadingWhite |
+                NumberStyles.AllowTrailingWhite |
+                NumberStyles.AllowLeadingSign |
+                NumberStyles.AllowDecimalPoint |
+                NumberStyles.AllowThousands |
+                NumberStyles.AllowExponent,
+                NumberFormatInfo.InvariantInfo, out result);
+    }
+}
---
+++ YTMAPI/Converters/ToDotNet.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+
+namespace YTMDotNet.YTMAPI.Converters {
+    static class ToDotNet {
+        #region Private Converters
+        private static object tryAll(dynamic value) {
+            if (tryBasic(value.ToString(), out object result1))
+                return result1;
+            else if (tryNamedDict(value, out Dictionary<string, object> result2))
+                return result2;
+            else if (tryUnnamedArray(value, out List<object> result3))
+                return result3;
+            else
+                return value.ToString();
+        }
+
+        private static bool tryBasic(string value, out object result) {
+            if (value == "None") {
+                result = null;
+                return true;
+            } else if (value == "True") {
+                result = true;
+                return true;
+            } else if (value == "False") {
+                result = false;
+                return true;
+            } else if (Helpers.TryParse(value, out int result2)) {
+                result = result2;
+                return true;
+            } else if (Helpers.TryParse(value, out ulong result3)) {
+                result = result3;
+                return true;
+            } else if (Helpers.TryParse(value, out double result4)) {
+                result = result4;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        private static Dictionary<string, object> convertNamedDict(dynamic value) {
+            Dictionary<string, object> items = new();
+            foreach (string key in value)
+                items.Add(key, tryAll(value[key]));
+            return items;
+        }
+        private static bool tryNamedDict(dynamic value, out Dictionary<string, object> result) {
+            try {
+                if (!value.ToString().StartsWith("{")) {
+                    result = null;
+                    return false;
+                }
+                result = convertNamedDict(value);
+                return true;
+            } catch {
+                result = null;
+                return false;
+            }
+        }
+
+        private static List<object> convertUnnamedArray(dynamic value) {
+            List<object> items = new();
+            foreach (object item in value)
+                items.Add(tryAll(item));
+            return items;
+        }
+        private static bool tryUnnamedArray(dynamic value, out List<object> result) {
+            try {
+                if (!value.ToString().StartsWith("[")) {
+                    result = null;
+                    return false;
+                }
+                result = convertUnnamedArray(value);
+                return true;
+            } catch {
+                result = null;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
