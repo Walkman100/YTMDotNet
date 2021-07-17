ca64621568fc3f8a08ad3e926f74391c31090f3a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2021-07-17 20:15:22 +0200

    added YTMAPIDisposable class to wrap getting Python GIL and YTmusicAPI module import

---
+++ YTMAPI/YTMAPIDisposable.cs
@@ -0,0 +1,37 @@
+using System;
+using Python.Runtime;
+
+namespace YTMDotNet.YTMAPI {
+    static class PyYTMAPI {
+        public static YTMAPIDisposable Get() =>
+            new YTMAPIDisposable();
+    }
+
+    // Py.GIL() wrapper to get YTmusicAPI library at the same time
+    public class YTMAPIDisposable : IDisposable {
+        private readonly Py.GILState state;
+        public readonly dynamic API;
+        private bool isDisposed;
+
+        internal YTMAPIDisposable() {
+            state = Py.GIL();
+
+            dynamic YTMusicAPI = Py.Import("ytmusicapi");
+            API = YTMusicAPI.YTMusic(Helpers.Settings.HeadersPath);
+        }
+
+        public virtual void Dispose() {
+            if (this.isDisposed)
+                return;
+
+            state.Dispose();
+            GC.SuppressFinalize(this);
+            this.isDisposed = true;
+        }
+
+        ~YTMAPIDisposable() {
+            // copy exception from Py.GILState.~GILState, since you can't call finalizers in C#...
+            throw new InvalidOperationException("GIL must always be released, and it must be released from the same thread that acquired it.");
+        }
+    }
+}
