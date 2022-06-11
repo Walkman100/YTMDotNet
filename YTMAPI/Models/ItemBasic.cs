a09f27f694a223d0ff576d6ebb479df17d224257
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2022-06-11 15:52:54 +0200

    Added DebuggerDisplay attribute to base item class

--- YTMAPI/Models/ItemBasic.cs
+++ YTMAPI/Models/ItemBasic.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
+
 namespace YTMDotNet.YTMAPI.Models {
+    [DebuggerDisplay("{GetType().Name}: {Title}")]
     class ItemBasic {
         public string BrowseID;
         public string Title;
