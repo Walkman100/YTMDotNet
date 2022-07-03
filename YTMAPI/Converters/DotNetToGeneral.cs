afc6988dfcdfd3ed881dbecd717b4f796c673c1f
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2022-07-03 15:52:15 +0200

    fixed DotNetToGeneral converters not working on null inputs

--- YTMAPI/Converters/DotNetToGeneral.cs
+++ YTMAPI/Converters/DotNetToGeneral.cs
@@ -5,7 +5,7 @@ using YTMDotNet.YTMAPI.Models;
 namespace YTMDotNet.YTMAPI.Converters {
     static class DotNetToGeneral {
         public static List<Thumbnail> GetThumbnails(List<object> input) =>
-            input.Select(obj => obj as Dictionary<string, object>).Select(
+            input?.Select(obj => obj as Dictionary<string, object>).Select(
                 dict => new Thumbnail() {
                     URL = dict["url"] as string,
                     Width = (int)dict["width"],
@@ -14,7 +14,7 @@ namespace YTMDotNet.YTMAPI.Converters {
 
         /// <summary>Note this takes "name" and "id" field</summary>
         public static List<ItemBasic> GetSimpleItems(List<object> input) =>
-            input.Select(obj => obj as Dictionary<string, object>).Select(
+            input?.Select(obj => obj as Dictionary<string, object>).Select(
                 dict => new ItemBasic() {
                     Title = dict["name"] as string,
                     BrowseID = dict["id"] as string,
